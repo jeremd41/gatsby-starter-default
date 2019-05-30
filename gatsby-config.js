e3c447ad0d901acb38690001de1a828dbc533311
Jérémy DIARD <44467357+jeremd41@users.noreply.github.com>
2019-05-30 14:07:24 +0200

    Mise à zéro + création de footer + style css

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Chambre d'hôtes`,
+    description: `Templates site pour chambre d'hôte`,
+    author: `Jeremy DIARD`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
---
+++ src/components/footer.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Footer = () => (
+  <div>
+    © {new Date().getFullYear()}, Built by
+    {` `}
+    <a href="https://www.gatsbyjs.org">Jérémy DIARD</a>
+  </div>
+)
+
+export default Footer
